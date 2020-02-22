afc150fc1f69bc2b2fb749d260ada4de401e005c
Juan Zaragoza <zaragozajn@gmail.com>
2020-02-22 14:56:06 -0800

    Solutions to Q's 66,136

---
+++ Easy/plusone_66.swift
@@ -0,0 +1,20 @@
+//Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+func plusOne(_ digits: [Int]) -> [Int] {
+  var currentDigit = digits.count - 1
+  var digitsCopy = digits
+  
+  while(currentDigit > -1){
+      if digitsCopy[currentDigit] == 9{
+          digitsCopy[currentDigit] = 0
+          if currentDigit == 0 {
+              digitsCopy[currentDigit] = 1
+              digitsCopy.append(0)
+          }
+          currentDigit -= 1
+          continue
+      }
+      digitsCopy[currentDigit] += 1
+      break
+  }
+    return digitsCopy
+}
