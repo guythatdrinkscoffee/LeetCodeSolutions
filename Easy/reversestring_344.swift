863b6973e159fb31dd23945a73c51d8a5a702194
Juan Zaragoza <zaragozajn@gmail.com>
2020-02-21 16:14:15 -0800

    Solution to Q 344

---
+++ Easy/reversestring_344.swift
@@ -0,0 +1,14 @@
+//Write a function that reverses a string. The input string is given as an array of characters char[].
+func reverseString(_ s: inout [Character]) {
+    var i = 0
+    var j = s.count - 1 
+    var t: Character
+    
+    while(i < j){
+        t = s[i]
+        s[i] = s[j]
+        s[j] = t
+        i += 1
+        j -= 1 
+    }
+}
