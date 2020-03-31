a316457973daff0211071a7eba5f18b677de1104
Juan Zaragoza <zaragozajn@gmail.com>
2020-03-30 19:53:56 -0700

    Solution to Q 287

---
+++ Medium/findduplicate_287.swift
@@ -0,0 +1,30 @@
+/* Given an array nums containing n + 1 integers where each integer 
+is between 1 and n (inclusive), prove that at least one duplicate number
+must exist. Assume that there is only one duplicate number, find the duplicate 
+one. 
+*/
+func findDuplicate(_ nums: [Int]) -> Int {
+    //Iteratively using two indeces
+    // var j = nums.count - 1
+    // for i in 0..<nums.count {
+    //     while i < j {
+    //         if nums[i] == nums[j] {
+    //             return nums[i]
+    //         }
+    //         j -= 1
+    //     }
+    //     j = nums.count - 1
+    // }
+
+    //Using a Set
+    var observed = Set<Int>()
+    for n in nums {
+        if observed.contains(n){
+            return n
+        } else {
+            observed.insert(n)
+        }
+    }
+
+    return -1
+}
