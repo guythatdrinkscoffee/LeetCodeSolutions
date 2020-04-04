5b4c1855c8a63c872eafa065e5e4e02e432ca5fb
Juan Zaragoza <zaragozajn@gmail.com>
2020-04-04 16:58:23 -0700

    Solutions to Q's 700,701

---
+++ Easy/searchinbst_700.swift
@@ -0,0 +1,26 @@
+/* Given the root node of a binary search tree (BST) and a value. 
+You need to find the node in the BST that the node's value equals 
+the given value. Return the subtree rooted with that node. If such 
+node doesn't exist, you should return NULL. */
+func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+    var nodeStack = [TreeNode]()
+    var currentNode = root 
+    var subTreeRoot: TreeNode?
+    
+    nodeStack.append(currentNode!)
+    while !nodeStack.isEmpty{
+        currentNode = nodeStack.popLast()
+        if currentNode!.val == val {
+            subTreeRoot = currentNode
+            return subTreeRoot
+        }
+        
+        if val < currentNode!.val && currentNode?.left != nil {
+            nodeStack.append((currentNode!.left!))
+        } else if val > currentNode!.val && currentNode?.right != nil {
+            nodeStack.append((currentNode!.right!))
+        }
+    }
+    
+    return nil
+}
