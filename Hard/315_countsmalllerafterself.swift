36fa5a35921e1cd7dd770a7aa1f552f3df39d3cf
Juan Zaragoza <zaragozajn@gmail.com>
2020-04-14 21:00:06 -0700

    Solution to Q 315

---
+++ Hard/315_countsmalllerafterself.swift
@@ -0,0 +1,26 @@
+/* You are given an integer array nums and you have to return a new counts array. 
+The counts array has the property where counts[i] is the number of smaller elements 
+to the right of nums[i]. */
+func countSmaller(_ nums: [Int]) -> [Int] {
+    var i = 0
+    var j = nums.count - 1
+    var out = [Int]()
+    var count = 0
+    
+    while i < nums.count {
+        while j > i {
+            if nums[j] < nums[i]{
+                count += 1
+            }
+            j -= 1
+        }
+        
+        out.append(count)
+        count = 0
+        j = nums.count - 1
+        i += 1
+    
+    }
+    
+    return out
+}
