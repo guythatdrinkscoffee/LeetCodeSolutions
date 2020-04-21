db586c7be1c6e3da3eae18c3b2b701c3711157b9
Juan Zaragoza <zaragozajn@gmail.com>
2020-04-20 21:59:36 -0700

    Fixed Typos

--- Easy/682_baseballgame.swift
+++ Easy/682_baseballgame.swift
@@ -7,3 +7,36 @@ Given a list of strings, each string can be one of the 4 following types:
 Each round's operation is permanent and could have an impact on the round before and the round after.
 
 You need to return the sum of the points you could get in all the rounds. */
+
+func calPoints(_ ops: [String]) -> Int {
+    var s = [Int]()
+    var sum = 0
+
+    for op in ops{
+        if op == "+"{
+            if let first = s.popLast(), let second = s.popLast(){
+                s.append(second)
+                s.append(first)
+                s.append(second + first)
+                sum += (second + first)
+            }
+        } else if op == "D" {
+            if let top = s.last {
+                sum += (top * 2)
+                s.append(top * 2)
+            }
+        } else if op == "C" {
+            if let top = s.last{
+                sum -= top
+                s.removeLast()
+            }
+        } else {
+            if let num = Int(op){
+                s.append(num)
+                sum += num
+            }
+        }
+    }
+
+    return sum
+}
