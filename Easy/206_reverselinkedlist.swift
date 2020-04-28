b938344fca3a52dbc97903a46c40d75a936f4d4d
Juan Zaragoza <zaragozajn@gmail.com>
2020-04-28 00:33:18 -0700

    Added Recursive Solution

--- Easy/206_reverselinkedlist.swift
+++ Easy/206_reverselinkedlist.swift
@@ -1,17 +1,30 @@
 //Reverse a singly linked list.
 func reverseList(_ head: ListNode?) -> ListNode? {
-    var newHead = head 
-    var previous = head
-    var current = head?.next
-    previous?.next = nil
+    //ITERATIVE
+    // var newHead = head 
+    // var previous = head
+    // var current = head?.next
+    // previous?.next = nil
     
-    while current != nil {
-        let next = current?.next
-        current?.next = previous
-        previous = current
-        current = next 
+    // while current != nil {
+    //     let next = current?.next
+    //     current?.next = previous
+    //     previous = current
+    //     current = next 
+    // }
+    
+    // newHead = previous
+    // return newHead
+
+    //RECURSIVE
+    if head == nil || head?.next == nil {
+        return head
     }
+
+    let list = reverseList(head?.next)
+
+    head?.next?.next = head
+    head?.next = nil
     
-    newHead = previous
-    return newHead 
+    return list 
 }
--- Easy/700_searchinbst.swift
+++ Easy/700_searchinbst.swift
@@ -3,24 +3,41 @@ You need to find the node in the BST that the node's value equals
 the given value. Return the subtree rooted with that node. If such 
 node doesn't exist, you should return NULL. */
 func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
-    var nodeStack = [TreeNode]()
-    var currentNode = root 
-    var subTreeRoot: TreeNode?
+    //ITERATIVE 
+    // var nodeStack = [TreeNode]()
+    // var currentNode = root 
+    // var subTreeRoot: TreeNode?
     
-    nodeStack.append(currentNode!)
-    while !nodeStack.isEmpty{
-        currentNode = nodeStack.popLast()
-        if currentNode!.val == val {
-            subTreeRoot = currentNode
-            return subTreeRoot
+    // nodeStack.append(currentNode!)
+    // while !nodeStack.isEmpty{
+    //     currentNode = nodeStack.popLast()
+    //     if currentNode!.val == val {
+    //         subTreeRoot = currentNode
+    //         return subTreeRoot
+    //     }
+        
+    //     if val < currentNode!.val && currentNode?.left != nil {
+    //         nodeStack.append((currentNode!.left!))
+    //     } else if val > currentNode!.val && currentNode?.right != nil {
+    //         nodeStack.append((currentNode!.right!))
+    //     }
+    // }
+    
+    // return nil
+
+    //RECURSIVE
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        guard let root = root else {
+            return nil
         }
         
-        if val < currentNode!.val && currentNode?.left != nil {
-            nodeStack.append((currentNode!.left!))
-        } else if val > currentNode!.val && currentNode?.right != nil {
-            nodeStack.append((currentNode!.right!))
+        if root.val == val {
+            return root
+        }else if val < root.val {
+            return searchBST(root.left, val)
         }
+        
+        return searchBST(root.right,val)
     }
-    
-    return nil
+
 }
