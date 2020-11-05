7ce5132f5a5098c5eea43e02aa68388a0a205884
Juan Zaragoza <zaragozajn@gmail.com>
2020-11-04 22:54:54 -0800

    Update 1470_shufflearray.swift

--- Easy/1470_shufflearray.swift
+++ Easy/1470_shufflearray.swift
@@ -1,18 +1,18 @@
-    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
-        var arr1  = [Int]()
-        var arr2 = [Int]()
-        var ans = [Int]()
+func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+    var arr1  = [Int]()
+    var arr2 = [Int]()
+    var ans = [Int]()
         
-        arr1.append(contentsOf: nums[0..<nums.count/2])
-        arr2.append(contentsOf: nums[(nums.count/2)...])
+    arr1.append(contentsOf: nums[0..<nums.count/2])
+    arr2.append(contentsOf: nums[(nums.count/2)...])
         
-        var i = 0 
-    
-        while( i < arr1.count && i < arr2.count){
-            ans.append(arr1[i])
-            ans.append(arr2[i])
-            i+=1
-        }
-        
-        return ans
+    var i = 0 
+
+    while( i < arr1.count && i < arr2.count){
+        ans.append(arr1[i])
+        ans.append(arr2[i])
+        i+=1
     }
+    
+    return ans
+}
